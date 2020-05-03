c583754632d7c5041ef6418c6329174d183ab05c
naoto-k-0917 <n.koizumi0917@gmail.com>
2020-05-03 18:26:59 +0900

    修正確認

--- code/django_web_front/src/xeno/index.js
+++ code/django_web_front/src/xeno/index.js
@@ -520,7 +520,7 @@ class Game extends React.Component {
         if (step === 0) {
             return (
                 <div className="cover">
-                    <Helmet title="Xeno" />
+                    <Helmet title="XENO" />
                     <GameStart
                         gameStartClick={() => this.gameStartClick()}
                     />
